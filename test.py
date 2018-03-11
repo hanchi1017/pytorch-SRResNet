22256f20b146b9d166e2f5aa7ab112d874f71e71
hanchi1017 <549407387@qq.com>
2018-03-11 22:10:50 +0800

    add delete files auto

--- test.py
+++ test.py
@@ -9,7 +9,8 @@ import scipy.misc as smi
 import matplotlib.pyplot as plt
 from dataset import get_test_set
 from PIL import Image
-from os.path import join
+from os.path import join, isdir, isfile
+from os import listdir, remove
 
 parser = argparse.ArgumentParser(description="PyTorch SRResNet Test")
 parser.add_argument("--cuda", action="store_true", help="use cuda?")
@@ -27,6 +28,14 @@ def PSNR(pred, gt, shave_border=0):
         return 100
     return 20 * math.log10(255.0 / rmse)
 
+def delete_all(path_list):
+    for path in path_list:
+        if isfile(path):
+            remove(path)
+        elif isdir(path):
+            for x in listdir(path):
+                remove(join(path, x))
+
 opt = parser.parse_args()
 cuda = opt.cuda
 
@@ -40,15 +49,26 @@ model = torch.load(opt.model)["model"]
 # im_l = sio.loadmat("Set5/" + opt.image + ".mat")['im_l']
 
 # test_dir = 'data/testset'   # 包含原始gt的image
+# hr_size = (128, 128)
 test_dir = 'data/other_method/img_gt'
+hr_size = (320,240)     # (height, width)
+
 gt_dir = 'result/img_gt'
 lr_dir = 'result/img_lr'
 hr_dir = 'result/img_hr'
 bi_dir = 'result/img_bi'
-hr_size = (320,240)     # (height, width)
+gt_dir_grey = 'result/img_gt_grey'
+lr_dir_grey = 'result/img_lr_grey'
+hr_dir_grey = 'result/img_hr_grey'
+bi_dir_grey = 'result/img_bi_grey'
+list_txt = 'result/list.txt'
+
+path_list = [gt_dir, lr_dir, hr_dir, bi_dir, gt_dir_grey, lr_dir_grey, hr_dir_grey, bi_dir_grey, list_txt]
+delete_all(path_list)
+
 testset = get_test_set(test_dir, hr_size, upscale_factor=4, quality=40)
 for i in range(0, testset.__len__()):
-    with open('result/list.txt','a') as f:
+    with open(list_txt,'a') as f:
         f.write('{} {}.png\n'.format(i,i))
 
     im_l, im_gt = testset.__getitem__(i)
@@ -90,15 +110,21 @@ for i in range(0, testset.__len__()):
     # 转换为灰度图，计算PSNR（即只计算Y分量）
     # im_gt_grey = Image.fromarray(im_gt)
     im_gt_grey = np.array(smi.toimage(im_gt).convert('L'))  # 函数toimage 将numpy array转为PIL.Image.Image, 或者使用Image.fromarray
-    im_b_grey = np.array(smi.toimage(im_b).convert('L'))
-    im_h_grey = np.array(smi.toimage(im_h).convert('L'))
+    im_bi_grey = np.array(smi.toimage(im_b).convert('L'))
+    im_hr_grey = np.array(smi.toimage(im_h).convert('L'))
+    im_lr_grey = np.array(smi.toimage(im_lr_norm).convert('L'))
+    smi.imsave(join(gt_dir_grey, '{}.png'.format(i)), im_gt_grey)
+    smi.imsave(join(bi_dir_grey, '{}.png'.format(i)), im_bi_grey)
+    smi.imsave(join(hr_dir_grey, '{}.png'.format(i)), im_hr_grey)
+    smi.imsave(join(lr_dir_grey, '{}.png'.format(i)), im_lr_grey)
+    print('images are saved')
 
-    psnr_bicubic = PSNR(im_b_grey, im_gt_grey)
-    psnr_output = PSNR(im_h_grey, im_gt_grey)
+    psnr_bicubic = PSNR(im_bi_grey, im_gt_grey)
+    psnr_output = PSNR(im_hr_grey, im_gt_grey)
 
-    # print("{}th image, Scale={}".format(i, opt.scale))
+    print("{}th image, Scale={}".format(i, opt.scale))
     print("It takes {}s for processing".format(elapsed_time))
-    print("bicubic psnr : {} ; net output psnr : {}\n".format(psnr_bicubic, psnr_output))
+    print("bicubic psnr : {} ; ours psnr : {}\n".format(psnr_bicubic, psnr_output))
 
     # fig = plt.figure()
     # ax = plt.subplot("131")
@@ -107,9 +133,9 @@ for i in range(0, testset.__len__()):
     #
     # ax = plt.subplot("132")
     # ax.imshow(im_b)
-    # ax.set_title("Input(Bicubic)")
+    # ax.set_title("Output(Bicubic)\nPSNR={}".format(psnr_bicubic))
     #
     # ax = plt.subplot("133")
     # ax.imshow(im_h)
-    # ax.set_title("Output(Net)")
+    # ax.set_title("Output(Net)\nPSNR={}".format(psnr_output))
     # plt.show()
