c546d18781d4333022683f27938a724f40857814
hanchi1017 <549407387@qq.com>
2018-05-09 21:21:21 +0800

    some fix

--- utils.py
+++ utils.py
@@ -1,10 +1,14 @@
+# coding=utf-8
+from __future__ import print_function
 import numpy as np
 import math
-from os.path import isdir, isfile, join
-from os import remove, listdir
+import os
+from os.path import isdir, isfile, join, exists
+from os import remove, listdir, mkdir
 import cv2
 import dlib
 from PIL import Image
+import time
 
 def PSNR(pred, gt, shave_border=0):
     height, width = pred.shape[:2]
@@ -17,6 +21,9 @@ def PSNR(pred, gt, shave_border=0):
     return 20 * math.log10(255.0 / rmse)
 
 def delete_all(path_list):
+    for path in path_list[:-1]:
+        if not exists(path):
+            mkdir(path)
     for path in path_list:
         if isfile(path):
             remove(path)
@@ -24,18 +31,15 @@ def delete_all(path_list):
             for x in listdir(path):
                 remove(join(path, x))
 
-def get_mask(img):
-    detector = dlib.get_frontal_face_detector()
-    landmark_predictor = dlib.shape_predictor('/media/lab/data/hanchi/dlib/shape_predictor_68_face_landmarks.dat')
+def get_mask(img, face_detector, landmark_predictor):
 
     img = np.array(img)
     mask = np.zeros(img.shape[0:2], dtype=np.float32)
-    # print 'mask shape: ',mask.shape
     r = img[:, :, 0]
     g = img[:, :, 1]
     b = img[:, :, 2]
     img = cv2.merge([b, g, r])
-    faces = detector(img, 1)
+    faces = face_detector(img, 1)
 
     if(len(faces)>0):
         for k,d in enumerate(faces):
@@ -68,14 +72,47 @@ def get_mask(img):
             mask[mouth[:,1].min():mouth[:,1].max(), mouth[:,0].min():mouth[:,0].max()] = 1
             mask[left_eye[:,1].min():left_eye[:,1].max(), left_eye[:,0].min():left_eye[:,0].max()] = 1
             mask[right_eye[:,1].min():right_eye[:,1].max(), right_eye[:,0].min():right_eye[:,0].max()] =1
-    # ret,thresh = cv2.threshold(mask, 0.5, 255, cv2.THRESH_BINARY_INV)
+    ret,thresh = cv2.threshold(mask, 0.5, 255, cv2.THRESH_BINARY_INV)
     # print type(thresh)
     # cv2.imshow('jj', thresh)
     # cv2.waitKey(0)
-    return mask
+    # return mask
 
+def is_image_file(filename):
+    return any(filename.endswith(extension) for extension in ['.png', '.jpg', '.jpeg'])
+
+def save_mask_batch(img_dir, mask_dir):
+    for x in listdir(img_dir):
+        start_time = time.time()
+        if is_image_file(x):
+            img = Image.open(join(img_dir, x))
+            mask = get_mask(img, face_detector, landmark_predictor)
+            np.save(join(mask_dir, os.path.splitext(x)[0]), mask)
+            elapsed_time = time.time() - start_time
+            # print('{} is saved, spent {}s'.format(x, elapsed_time))
+
+face_detector = dlib.get_frontal_face_detector()
+landmark_predictor = dlib.shape_predictor('/media/lab/data/hanchi/dlib/shape_predictor_68_face_landmarks.dat')
 
 if __name__ == "__main__":
-    filepath = '/media/lab/data/hanchi/PycharmProjects/pytorch-SRResNet/data/face2.jpg'
-    img = Image.open(filepath)
-    print get_mask(img).shape
+    # filepath = 'data/testset_glass'
+    # img = Image.open(filepath)
+    # mask = get_mask(img, face_detector, landmark_predictor)
+    # print(mask.shape)
+
+    img = cv2.imread('data/testset_glass/002058.jpg')
+    faces = face_detector(img, 1)
+    if (len(faces) > 0):
+        for k, d in enumerate(faces):
+            cv2.rectangle(img, (d.left(), d.top()), (d.right(), d.bottom()), (255, 255, 255))
+            shape = landmark_predictor(img, d)
+            for i in range(68):
+                cv2.circle(img, (shape.part(i).x, shape.part(i).y), 1, (0, 255, 0), -1, 8)
+                # cv2.putText(img, str(i), (shape.part(i).x, shape.part(i).y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 2555, 255))
+    cv2.imshow('Frame', img)
+    cv2.waitKey(0)
+
+    # img_dir = '/media/lab/data/hanchi/PycharmProjects/pytorch-SRResNet/data/img_align_celeba'
+    # # img_dir = '/media/lab/data/hanchi/PycharmProjects/pytorch-SRResNet/data/other_method/img_gt'
+    # mask_dir = '/media/lab/data/hanchi/PycharmProjects/pytorch-SRResNet/data/img_align_celeba_mask'
+    # save_mask_batch(img_dir, mask_dir)
