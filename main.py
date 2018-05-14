90e06aa24e9bc3a2e6ae50c73592a231d6e3098e
hanchi1017 <549407387@qq.com>
2018-05-14 16:07:19 +0800

    improve the result recording

--- main.py
+++ main.py
@@ -37,25 +37,34 @@ parser.add_argument("--vgg_loss", action="store_true", help="Use content loss?")
 opt = parser.parse_args()
 print(opt)
 
+# train mode
 train_dir = "data/train"
 test_dir = "data/test"
 crop_size = (128, 128)
-cpt_dir = "model/checkpoint_vgg_relu22"
+cpt_dir = "model/checkpoint_vgg_relu22_2"
 beta = 1
+
+# debug mode
+# train_dir = "data/minitest2"
+# test_dir = "data/minitest2"
+# crop_size = (128, 128)
+# cpt_dir = "model/test"
+# beta = 1
 
 if not os.path.exists(cpt_dir):
     os.makedirs(cpt_dir)
-resultFile = open("{}/result.csv".format(cpt_dir), 'a')
-fieldnames = ['Epoch', 'Iteration', 'AvgPSNR', 'AvgCustomLoss']
-resultWriter = csv.DictWriter(resultFile, fieldnames=fieldnames)
-resultWriter.writeheader()
-writer = SummaryWriter()
+evalResult = open("{}/eval_result.csv".format(cpt_dir), 'a')
+trainResult = open("{}/train_result.csv".format(cpt_dir), 'a')
+fieldnames = ['Epoch', 'Iteration','AvgPSNR', 'AvgCustomLoss', 'AvgContentLoss', 'lr', 'batch_num', 'seed']
+evalWriter = csv.DictWriter(evalResult, fieldnames=fieldnames)
+trainWriter = csv.DictWriter(trainResult, fieldnames=fieldnames)
+evalWriter.writeheader()
+trainWriter.writeheader()
+SummWriter = SummaryWriter()
 
 def main():
 
-
     global opt, model, netContent
-
 
     cuda = opt.cuda
     if cuda and not torch.cuda.is_available():
@@ -94,13 +103,14 @@ def main():
     print("===> Building model")
     model = Net()
 
-    mseLoss = nn.MSELoss(size_average=False)
-    criterion = CustomLoss(beta)
+    # mseLoss = nn.MSELoss(size_average=False)
+    mseLoss = nn.MSELoss()
+    customLoss = CustomLoss(beta)
 
     print("===> Setting GPU")
     if cuda:
         model = model.cuda()
-        criterion = criterion.cuda()
+        customLoss = customLoss.cuda()
         if opt.vgg_loss:
             netContent = netContent.cuda()
             mseLoss.cuda()
@@ -133,18 +143,18 @@ def main():
 
     for epoch in range(opt.start_epoch, opt.nEpochs + 1):
         scheduler.step()
-        train(training_data_loader, test_data_loader, optimizer, model, criterion, mseLoss, epoch, opt.seed, cpt_dir)
+        train(training_data_loader, test_data_loader, optimizer, model, customLoss, mseLoss, epoch, opt.seed, cpt_dir)
         save_checkpoint(model, epoch, optimizer.param_groups[0]['lr'], cpt_dir)
 
-    writer.close()
-    resultFile.close()
+    SummWriter.close()
+    evalResult.close()
 
 def adjust_learning_rate(epoch):
     """Sets the learning rate to the initial LR decayed by 10"""
     lr = opt.lr * (0.1 ** (epoch // opt.step))  # 设置学习率衰减规则，每opt.step个epoch 学习率减小10倍
     return lr    
 
-def train(training_data_loader, test_data_loader, optimizer, model, criterion, mseLoss, epoch, seed, cpt_dir):
+def train(training_data_loader, test_data_loader, optimizer, model, customLoss, mseLoss, epoch, seed, cpt_dir):
 
     # lr = adjust_learning_rate(epoch-500-1)
     #
@@ -165,7 +175,7 @@ def train(training_data_loader, test_data_loader, optimizer, model, criterion, m
             hr_mask = hr_mask.cuda()
 
         output = model(input)
-        loss = criterion(input, output, target, lr_mask, hr_mask)
+        loss = customLoss(input, output, target, lr_mask, hr_mask)
 
         if opt.vgg_loss:
             content_input = netContent(output)
@@ -183,14 +193,21 @@ def train(training_data_loader, test_data_loader, optimizer, model, criterion, m
 
         optimizer.step()
         elapsed_time = time.time() - start_time
-        if iteration%100 == 0:
 
+        n_iter = (epoch-opt.start_epoch)*len(training_data_loader)+iteration
+        if iteration%100 == 0:
             if opt.vgg_loss:
                 print("===> Epoch[{}]({}/{}): Loss: {:.10f} Content_loss {:.10f}".format(epoch, iteration, len(training_data_loader), loss.data[0], content_loss.data[0]))
             else:
                 print("===> Epoch[{}]({}/{}): Loss: {:.10f} time: {}".format(epoch, iteration, len(training_data_loader), loss.data[0], elapsed_time))
-            evaluate(test_data_loader, model, criterion, opt.seed, epoch, (epoch-opt.start_epoch)*len(training_data_loader)+iteration)
-
+        if iteration%1 == 0:
+            # writer.add_graph(model, *(input, target))
+            for name, param in model.named_parameters():
+                SummWriter.add_histogram("seed_{}/{}".format(seed, name), param.grad.data.cpu(), n_iter)
+            evaluate(test_data_loader, model, customLoss, mseLoss, opt.seed, epoch, iteration, optimizer.param_groups[0]["lr"], )
+            evalWriter.writerow({'Epoch': epoch, 'Iteration': iteration, 'AvgCustomLoss': loss.data[0],
+                                 'AvgContentLoss': content_loss.data[0], 'lr': optimizer.param_groups[0]["lr"], 'batch_num': len(test_data_loader),
+                                 'seed': seed})
 
 def save_checkpoint(model, epoch, lr, cpt_dir):
     model_out_path = "{}/base500_model_epoch_{}_lr_{}.pth".format(cpt_dir, epoch, lr)
@@ -202,34 +219,44 @@ def save_checkpoint(model, epoch, lr, cpt_dir):
         
     print("Checkpoint saved to {}".format(model_out_path))
 
-def evaluate(test_data_loader, model, criterion, seed, epoch, iteration):
+def evaluate(test_data_loader, model, CustomLoss, MSELoss, seed, epoch, iteration, lr, ):
 
     avg_psnr = 0
     avg_loss = 0
+    avg_content_loss = 0
     for iterarion, batch in enumerate(test_data_loader, 1):
-        input, target, lr_mask, hr_mask = Variable(batch[0]), Variable(batch[1], requires_grad=False), Variable(batch[2], requires_grad=False), Variable(batch[3], requires_grad=False)
-        MSELoss = nn.MSELoss()
+        input, target, lr_mask, hr_mask = Variable(batch[0], volatile=True), Variable(batch[1], volatile=True), \
+                                          Variable(batch[2], volatile=True), Variable(batch[3], volatile=True)
+
         if opt.cuda:
             input = input.cuda()
             target = target.cuda()
             lr_mask = lr_mask.cuda()
             hr_mask = hr_mask.cuda()
-            MSELoss = MSELoss.cuda()
 
         output = model(input)
-        loss = criterion(input, output, target, lr_mask, hr_mask)
+        loss = CustomLoss(input, output, target, lr_mask, hr_mask)
         avg_loss += loss.data[0]
 
         mse = MSELoss(output, target)
         psnr = 10 * math.log10(1 / mse.data[0])
         avg_psnr += psnr
 
-    writer.add_scalar("seed_{}/CustomLoss_mean".format(seed), avg_loss, epoch)
-    writer.add_scalar("seed_{}/psnr_mean".format(seed), avg_psnr, epoch)
-    resultWriter.writerow({'Epoch':epoch, 'Iteration':iteration, 'AvgPSNR':avg_psnr, 'AvgCustomLoss':avg_loss})
+        if opt.vgg_loss:
+            content_input = netContent(output)
+            content_target = netContent(target)
+            content_loss = MSELoss(content_input, content_target)
+            avg_content_loss += content_loss.data[0]
 
+    avg_loss /= len(test_data_loader)
+    avg_psnr /= len(test_data_loader)
+    avg_content_loss /= len(test_data_loader)
+    SummWriter.add_scalar("seed_{}/CustomLoss_mean".format(seed), avg_loss, epoch)
+    SummWriter.add_scalar("seed_{}/psnr_mean".format(seed), avg_psnr, epoch)
+    evalWriter.writerow({'Epoch':epoch, 'Iteration':iteration, 'AvgPSNR':avg_psnr, 'AvgCustomLoss':avg_loss,
+                           'AvgContentLoss':avg_content_loss, 'lr':lr, 'batch_num':len(test_data_loader), 'seed':seed})
     if iteration%1000 == 0:
-        print("seed {}: ===> Epoch: {}; Iteration: {}; Avg. CustomLoss: {:.4f}; Avg. PSNR: {:.4f} dB".format(seed,epoch,iteration,avg_loss / len(test_data_loader), avg_psnr / len(test_data_loader)))   # len(test_data_loader) 为batch总数
+        print("seed {}: ===> Epoch: {}; Iteration: {}; Avg. CustomLoss: {:.4f}; Avg. PSNR: {:.4f} dB".format(seed,epoch,iteration,avg_loss,avg_psnr))   # len(test_data_loader) 为batch总数
 
 class CustomLoss(nn.Module):
     def __init__(self, beta):
